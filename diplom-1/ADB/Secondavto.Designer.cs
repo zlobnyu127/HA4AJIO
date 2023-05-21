49bf4dddc71eba29c32ea0e20250aa40365ed4a8
zlobnyu127 <117861149+zlobnyu127@users.noreply.github.com>
2023-05-21 17:32:23 +0300

    Add files via upload

---
+++ diplom-1/ADB/Secondavto.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ADB
+{
+    partial class Secondavto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(175, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(422, 222);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(28, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Название";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(28, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Двигатель";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(28, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Комплектация";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(28, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Цвет";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(28, 247);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Цена";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(87, 270);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(204, 73);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "компл";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(97, 352);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 23);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "транс";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(603, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(700, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Secondavto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 465);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Secondavto";
+            this.Text = "Secondavto";
+            this.Load += new System.EventHandler(this.Secondavto_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ diplom-1/ADB/Secondavto.cs
@@ -0,0 +1,213 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace ADB
+{
+    public partial class Secondavto : Form
+    {
+        public Secondavto()
+        {
+            InitializeComponent();
+        }
+        string q = "";
+        int x = 0;
+        int sum = 0;
+        
+        
+        
+        private void Secondavto_Load(object sender, EventArgs e)
+        {
+            DB db = new DB();
+            db.openConnection();
+            MySqlCommand command = new MySqlCommand("Select av_foto from avtomobiles WHERE av_name = @id.n ", db.getConnection());
+            command.Parameters.Add("@id.n", MySqlDbType.String).Value = id.n;
+            MySqlDataReader Reader = command.ExecuteReader();
+            Reader.Read();
+            if (Reader.HasRows)
+            {
+
+                byte[] img = (byte[])(Reader[0]);
+                if (img == null)
+                {
+                    pictureBox1.Image = null;
+                }
+                else
+                {
+
+                    MemoryStream ms = new MemoryStream(img);
+                    pictureBox1.Image = Image.FromStream(ms);
+                }
+            }
+            else { MessageBox.Show(" Конец "); }
+            db.closeConnection();
+            DB d = new DB();
+            d.openConnection();
+            MySqlCommand comman = new MySqlCommand("Select av_name,av_dvig,av_complectaction,av_c_opic,av_color,av_trans,av_price,av_id FROM avtomobiles WHERE av_name = @id.n and av_status = 'В наличии'", d.getConnection());
+            comman.Parameters.Add("@id.n", MySqlDbType.String).Value = id.n;
+            MySqlDataReader reade = comman.ExecuteReader();
+            reade.Read();
+            if (reade.HasRows)
+            {
+                label1.Text = reade[0].ToString();
+                label2.Text = reade[1].ToString();
+                label3.Text = reade[2].ToString();
+                label7.Text = reade[3].ToString();
+                label4.Text = reade[4].ToString();
+                label9.Text = reade[5].ToString();
+                label5.Text = reade[6].ToString();
+                sum = Int32.Parse(reade[7].ToString());
+                
+                
+            }
+            
+            d.closeConnection();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (x != 0 )
+            {
+                x--;
+                sum--;
+                while (true)
+                {
+                    DB db = new DB();
+                    db.openConnection();
+                    MySqlCommand command = new MySqlCommand("Select av_name, av_dvig, av_complectaction, av_c_opic, av_color, av_trans, av_price FROM avtomobiles WHERE av_id = @sum and av_status = 'В наличии'", db.getConnection());
+                    command.Parameters.Add("@sum", MySqlDbType.String).Value = sum;
+                    MySqlDataReader reader = command.ExecuteReader();
+                    reader.Read();
+                    if (reader.HasRows)
+                    {
+
+                        label1.Text = reader[0].ToString();
+                        label2.Text = reader[1].ToString();
+                        label3.Text = reader[2].ToString();
+                        label7.Text = reader[3].ToString();
+                        label4.Text = reader[4].ToString();
+                        label9.Text = reader[5].ToString();
+                        label5.Text = reader[6].ToString();
+                        DB d = new DB();
+                        d.openConnection();
+                        MySqlCommand comman = new MySqlCommand("Select av_foto from avtomobiles WHERE av_id = @sum ", d.getConnection());
+                        comman.Parameters.Add("@sum", MySqlDbType.Int32).Value = sum;
+                        MySqlDataReader Reade = comman.ExecuteReader();
+                        Reade.Read();
+                        if (Reade.HasRows)
+                        {
+
+                            byte[] img = (byte[])(Reade[0]);
+                            if (img == null)
+                            {
+                                pictureBox1.Image = null;
+                            }
+                            else
+                            {
+
+                                MemoryStream ms = new MemoryStream(img);
+                                pictureBox1.Image = Image.FromStream(ms);
+                            }
+                        }
+                        else { MessageBox.Show(" Конец "); }
+                        d.closeConnection();
+                    }
+                    if (q == id.n)
+                    {
+                        
+                        return;
+                    }
+                    else if (sum == 1) { return; }
+                    else { sum--; };
+
+                }
+            }
+            else { MessageBox.Show("Конец"); }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string y = "";
+            int count = 0;
+            DB d = new DB();
+            d.openConnection();
+            MySqlCommand comman = new MySqlCommand("SELECT COUNT(*) FROM avtomobiles", d.getConnection());
+            //comman.Parameters.Add("@id.n",MySqlDbType.String).Value = id.n;
+            MySqlDataReader reade = comman.ExecuteReader();
+            reade.Read();
+            if (reade.HasRows)
+            {
+                
+                y = reade[0].ToString();
+                 count = Int32.Parse(y);
+            }
+            if (x < count-1)
+            {
+                x++;
+                sum++;
+                while (true)
+                {
+                    DB db = new DB();
+                    db.openConnection();
+                    MySqlCommand command = new MySqlCommand("Select av_name, av_dvig, av_complectaction, av_c_opic, av_color, av_trans, av_price FROM avtomobiles WHERE av_id = @sum and av_status = 'В наличии'", db.getConnection());
+                    command.Parameters.Add("@sum", MySqlDbType.String).Value = sum;
+                    MySqlDataReader reader = command.ExecuteReader();
+                    reader.Read();
+                    if (reader.HasRows)
+                    {
+
+                        label1.Text = reader[0].ToString();
+                        label2.Text = reader[1].ToString();
+                        label3.Text = reader[2].ToString();
+                        label7.Text = reader[3].ToString();
+                        label4.Text = reader[4].ToString();
+                        label9.Text = reader[5].ToString();
+                        label5.Text = reader[6].ToString();
+                        DB dbd = new DB();
+                        dbd.openConnection();
+                        MySqlCommand commandd = new MySqlCommand("Select av_foto from avtomobiles WHERE av_id = @sum ", dbd.getConnection());
+                        commandd.Parameters.Add("@sum", MySqlDbType.Int32).Value = sum;
+                        MySqlDataReader Readedd = commandd.ExecuteReader();
+                        Readedd.Read();
+                        if (Readedd.HasRows)
+                        {
+
+                            byte[] img = (byte[])(Readedd[0]);
+                            if (img == null)
+                            {
+                                pictureBox1.Image = null;
+                            }
+                            else
+                            {
+
+                                MemoryStream ms = new MemoryStream(img);
+                                pictureBox1.Image = Image.FromStream(ms);
+                            }
+                        }
+                        else { MessageBox.Show(" Конец "); }
+                        dbd.closeConnection();
+                    }
+                    if (q == id.n)
+                    {
+                        
+                        return;
+                    }
+                    else if (sum > count + 1) { return; }
+                    else { sum++; };
+                }
+            }
+            else { MessageBox.Show("Конец"); }
+            
+        }
+    }
+}
